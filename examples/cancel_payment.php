11209a15037f6c834a29bad871455c42c78d3a7e
Mohamed Jasim Ahmed <34663944+MJAISE@users.noreply.github.com>
2024-07-17 15:38:10 +0300

    feat: Split example operations from index.php into separate files
    - Moved payment creation example to create_payment.php
    - Moved payment status check example to check_payment_status.php
    - Moved payment refund example to refund_payment.php
    - Moved payment cancellation example to cancel_payment.php

---
+++ examples/cancel_payment.php
@@ -0,0 +1,41 @@
+<?php
+  require_once __DIR__ . '/../vendor/autoload.php';
+  require_once __DIR__ . '/create_payment.php'; // Include the file with createPayment() function
+  
+  use Dotenv\Dotenv;
+  use FirstIraqiBank\FIBPaymentSDK\Services\FIBAuthIntegrationService;
+  use FirstIraqiBank\FIBPaymentSDK\Services\FIBPaymentIntegrationService;
+
+// Load environment variables from the .env file
+  $dotenv = Dotenv::createImmutable(__DIR__.'/..');
+  $dotenv->load();
+  
+  try {
+    // Get the payment ID
+    $paymentId = createPayment();
+    
+    // This $paymentId should be retrieved from your database or cache in real implementations
+    // Example: $paymentId = $databaseService->retrievePaymentId();
+    
+    // Initialize the authentication service
+    $authService = new FIBAuthIntegrationService();
+    
+    // Initialize the payment integration service
+    $paymentService = new FIBPaymentIntegrationService($authService);
+    
+    // cancel Payment
+    $response = $paymentService->cancel($paymentId);
+    // Check if the cancellation was successful
+    if (in_array($response->getStatusCode(), [200, 201, 202, 204])) {
+      echo "Cancel Payment Status: Successful\n";
+    } else {
+      echo "Cancel Payment Status: Failed with status code " . $response->getStatusCode() . "\n";
+    }
+    
+    
+    // Example: update payment details in a database or cache for real implementations
+    // $databaseService->updatePaymentDetails($paymentDetails);
+    
+  } catch (Exception $e) {
+    echo "Error Refunding payment: " . $e->getMessage() . PHP_EOL;
+  }
---
+++ examples/check_payment_status.php
@@ -0,0 +1,35 @@
+<?php
+  require_once __DIR__ . '/../vendor/autoload.php';
+  require_once __DIR__ . '/create_payment.php'; // Include the file with createPayment() function
+  
+  use Dotenv\Dotenv;
+  use FirstIraqiBank\FIBPaymentSDK\Services\FIBAuthIntegrationService;
+  use FirstIraqiBank\FIBPaymentSDK\Services\FIBPaymentIntegrationService;
+
+// Load environment variables from the .env file
+  $dotenv = Dotenv::createImmutable(__DIR__.'/..');
+  $dotenv->load();
+  
+  try {
+    // Get the payment ID
+    $paymentId = createPayment();
+    
+    // This $paymentId should be retrieved from your database or cache in real implementations
+    // Example: $paymentId = $databaseService->retrievePaymentId();
+    
+    // Initialize the authentication service
+    $authService = new FIBAuthIntegrationService();
+    
+    // Initialize the payment integration service
+    $paymentService = new FIBPaymentIntegrationService($authService);
+    
+    // Check Payment Status
+    $response = $paymentService->checkPaymentStatus($paymentId);
+    echo "Payment Status: " . $response['status'] ?? null . PHP_EOL;
+    
+    // Example: Store payment details in a database or cache for real implementations
+    // $databaseService->storePaymentDetails($paymentDetails);
+    
+  } catch (Exception $e) {
+    echo "Error checking payment status: " . $e->getMessage() . PHP_EOL;
+  }
